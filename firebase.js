307c1828b639e078467586e3a1873a41b3507666
yashodip <yashodip9@gmail.com>
2020-08-11 17:41:27 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,11 @@
+//add your own firebase config 
 var firebaseConfig = {
-    apiKey: "AIzaSyD2TJbUUKBKNuwjEBPTuIKdSIKBchqv52I",
-    authDomain: "spotify-clone-3cb2b.firebaseapp.com",
-    databaseURL: "https://spotify-clone-3cb2b.firebaseio.com",
-    projectId: "spotify-clone-3cb2b",
-    storageBucket: "spotify-clone-3cb2b.appspot.com",
-    messagingSenderId: "319111310996",
-    appId: "1:319111310996:web:e8614d7884f1256aa6ce81",
-    measurementId: "G-XZNS44BBZF"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
